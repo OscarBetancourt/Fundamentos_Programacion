bbd3ec8c727fe45dc0c7b3616ad10abfbb84c8f3
Oscar Betancourt <oscarbetancourt89@gmail.com>
2021-06-30 18:08:17 -0500

    prueba repositorio

---
+++ prueba/suerte/src/main/java/xyz/iam3mer/baorjs/app/App.java
@@ -0,0 +1,26 @@
+package xyz.iam3mer.baorjs.app;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Scanner input = new Scanner(System.in);
+        String fecha = input.nextLine();
+        int indice  = fecha.indexOf('/');
+        int dia = Integer.parseInt(fecha.substring(0,indice));
+        int indice2 = fecha.lastIndexOf('/');
+        int mes = Integer.parseInt(fecha.substring(++indice,indice2));
+        int anio = Integer.parseInt(fecha.substring(++indice2,fecha.length()));
+        int suerte = dia + mes + anio ;
+        System.out.println(mes);
+        input.close();
+
+    }
+}
